3a287b2e6be29621dbfef43aa47fb41886f62116
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-05-22 19:08:57 +0300

    redacting file

--- V2_dmlAndDql.sql
+++ V2_dmlAndDql.sql
@@ -12,7 +12,7 @@ VALUES (1, 'Белый', 'White', false),
        (2, '3 * 13', '39', false),
        (2, '100 / 25', '4', false);
 
---Удаление строки из таблицы titles
+--Удаление строки из таблицы flashcards-themes
 DELETE
 FROM flashCards_themes
 WHERE id = 2;
@@ -22,7 +22,7 @@ DELETE
 FROM flashcards
 WHERE id = 2;
 
--- Список набора карточек titles
+-- Список набора карточек flashcards_themes
 SELECT id       AS id,
        set_name AS name
 FROM flashCards_themes;
@@ -48,27 +48,8 @@ SELECT id               AS id,
 FROM flashcards
 WHERE flashCards_themes_id = 2;
 
---Расчет количество изученных карточек из всего списка карточек
+--Список карточек
 
-SELECT flashCards_themes.id                                              AS id,
-       flashCards_themes.set_name                                        AS name,
-       count(flashcards.id) FILTER ( WHERE flashcards.status_knowledge ) AS succsess,
-       count(flashcards.id)                                              AS global
-FROM flashCards_themes
-         LEFT JOIN flashcards ON flashCards_themes.id = flashcards.flashCards_themes_id
-GROUP BY flashCards_themes.id;
-
-
---Выбор карточки и выполнение тренировки
-SELECT id               AS id,
-       question         AS question,
-       answer           AS answer,
-       status_knowledge AS status_knowledge
-FROM flashcards
-WHERE flashcards.flashCards_themes_id = 1
-  AND NOT flashcards.status_knowledge
-ORDER BY flashcards.id
-LIMIT 1;
 
 SELECT id               AS id,
        question         AS question,
@@ -77,8 +58,19 @@ SELECT id               AS id,
 FROM flashcards
 WHERE flashcards_themes_id = 1
   AND NOT flashcards.status_knowledge
+  AND flashcards.id > 1
 ORDER BY flashcards.id
-LIMIT 1 OFFSET 1;
+LIMIT 1;
+
+--Расчет количество изученных карточек из всего списка карточек
+
+SELECT flashCards_themes.id                                              AS id,
+       flashCards_themes.set_name                                        AS name,
+       count(flashcards.id) FILTER ( WHERE flashcards.status_knowledge ) AS succsess,
+       count(flashcards.id)                                              AS global
+FROM flashCards_themes
+            LEFT JOIN flashcards ON flashCards_themes.id = flashcards.flashCards_themes_id
+GROUP BY flashCards_themes.id;
 
 
 --изменение колонки status_knowledge при успешном выполнении
