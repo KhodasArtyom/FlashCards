a69966ef029078dfbe00b26f4265c8dfbf8b68be
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-05-30 23:02:55 +0300

    add service flashCardsThemesImplementation

--- src/main/java/by/tms/courseProject2/FlashCards/service/FLashCardThemeServiceImpl.java
+++ src/main/java/by/tms/courseProject2/FlashCards/service/FLashCardThemeServiceImpl.java
@@ -1,11 +1,13 @@
 package by.tms.courseProject2.FlashCards.service;
 
+import by.tms.courseProject2.FlashCards.exception.ServiceException;
 import by.tms.courseProject2.FlashCards.models.FlashCardsThemes;
 import by.tms.courseProject2.FlashCards.repository.FlashCardsThemesRepository;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class FLashCardThemeServiceImpl implements FlashCardThemeService{
+public class FLashCardThemeServiceImpl implements FlashCardThemeService {
 
     private final FlashCardsThemesRepository flashCardsThemesRepository;
 
@@ -17,20 +19,26 @@ public class FLashCardThemeServiceImpl implements FlashCardThemeService{
 
     @Override
     public void addNewTheme(String name) {
-        if(!name.isEmpty()) {
+        if (!name.isEmpty()) {
             flashCardsThemesRepository.save(name);
         } else {
-            throw new
+            throw new ServiceException();
         }
     }
 
     @Override
     public void removeTheme(long flashCardThemeID) {
+        try {
+            flashCardsThemesRepository.remove(flashCardThemeID);
+        } catch (ServiceException e) {
+            throw new ServiceException();
+        }
 
     }
 
     @Override
     public List<FlashCardsThemes> seeAllThemes() {
-        return null;
+
+        return flashCardsThemesRepository.getAllThemes();
     }
 }
