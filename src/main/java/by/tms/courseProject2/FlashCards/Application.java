f181cce52ade5350b2172e49496e76950970adce
ArtyomKhodas <a.Khodas1601@gmail.com>
2023-05-30 20:28:21 +0300

    add Hikari datasource

--- src/main/java/by/tms/courseProject2/FlashCards/Application.java
+++ src/main/java/by/tms/courseProject2/FlashCards/Application.java
@@ -1,49 +1,55 @@
 package by.tms.courseProject2.FlashCards;
 
 import by.tms.courseProject2.FlashCards.models.FlashCards;
-import by.tms.courseProject2.FlashCards.models.FlashCardsThemes;
 import by.tms.courseProject2.FlashCards.repository.FlashCardsJDBCRepository;
 import by.tms.courseProject2.FlashCards.repository.FlashCardsRepository;
 import by.tms.courseProject2.FlashCards.repository.FlashCardsThemesJDBCRepository;
 import by.tms.courseProject2.FlashCards.repository.FlashCardsThemesRepository;
-import org.postgresql.ds.PGSimpleDataSource;
+import com.zaxxer.hikari.HikariDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.List;
 import java.util.Optional;
 
 public class Application {
+    private static final Logger log = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
-        PGSimpleDataSource dataSource = new PGSimpleDataSource();
 
-        dataSource.setURL(System.getenv("variableUrl"));
-        dataSource.setUser(System.getenv("variableUser"));
+        HikariDataSource dataSource = new HikariDataSource();
+        log.info("The program has started");
+        dataSource.setJdbcUrl(System.getenv("variableUrl"));
+        dataSource.setUsername(System.getenv("variableUser"));
         dataSource.setPassword(System.getenv("variablePassword"));
-        FlashCardsThemesRepository flashCardsThemesRepository = new FlashCardsThemesJDBCRepository(dataSource);
-        FlashCardsRepository flashCardsRepository = new FlashCardsJDBCRepository(dataSource);
+        try (HikariDataSource db = new HikariDataSource(dataSource)) {
+            FlashCardsThemesRepository flashCardsThemesRepository = new FlashCardsThemesJDBCRepository(dataSource);
+            FlashCardsRepository flashCardsRepository = new FlashCardsJDBCRepository(dataSource);
 
 
-        flashCardsThemesRepository.save("COLORS");
-        flashCardsThemesRepository.save("NUMBERS");
+            flashCardsThemesRepository.save("COLORS");
+            flashCardsThemesRepository.save("NUMBERS");
 
 
+            flashCardsRepository.save(1, "Black", "Чёрный", false);
+            flashCardsRepository.save(1, "White", "Белый", false);
+            flashCardsRepository.save(1, "Orange", "Оранжевый", false);
+            flashCardsRepository.save(1, "Green", "Зелёный", false);
+            flashCardsRepository.save(1, "Жёлтый", "Жёлтый", false);
 
-        flashCardsRepository.save(1, "Black", "Чёрный", false);
-        flashCardsRepository.save(1, "White", "Белый", false);
-        flashCardsRepository.save(1, "Orange", "Оранжевый", false);
-        flashCardsRepository.save(1, "Green", "Зелёный", false);
-        flashCardsRepository.save(1, "Жёлтый", "Жёлтый", false);
+            List<FlashCards> allByThemeId = flashCardsRepository.findAllByThemeId(1);
+            System.out.println(allByThemeId);
 
-        List<FlashCards> allByThemeId = flashCardsRepository.findAllByThemeId(1);
-        System.out.println(allByThemeId);
+            System.out.println("=========================");
 
-        System.out.println("=========================");
+            Optional<FlashCards> allFlashCardsByIdAndOffset = flashCardsRepository.findAllFlashCardsByIdAndOffset(1, 0);
+            System.out.println(allFlashCardsByIdAndOffset);
 
-        Optional<FlashCards> allFlashCardsByIdAndOffset = flashCardsRepository.findAllFlashCardsByIdAndOffset(1, 0);
-        System.out.println(allFlashCardsByIdAndOffset);
-
-        flashCardsRepository.statusUpdateLearned(1,true);
-        flashCardsRepository.statusUpdateLearned(2,true);
+            flashCardsRepository.statusUpdateLearned(1, true);
+            flashCardsRepository.statusUpdateLearned(2, true);
+            log.info("The program ended");
 
 
+        }
     }
 }
